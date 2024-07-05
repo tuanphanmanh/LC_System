6dc612bcfcc7d3d034336a69435f200b82684e55
tuanphanmanh <tuanphanmanh86@gmail.com>
2024-07-05 09:43:46 +0700

    a

---
+++ aspnet-core/src/MyCompanyName.AbpZeroTemplate.GraphQL/Configure/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using GraphQL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using MyCompanyName.AbpZeroTemplate.Debugging;
+
+namespace MyCompanyName.AbpZeroTemplate.Configure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAndConfigureGraphQL(this IServiceCollection services)
+        {
+            services
+                .AddGraphQL(x => x.AddSystemTextJson()
+                    .AddErrorInfoProvider(opt => opt.ExposeExceptionDetails = DebugHelper.IsDebug)
+                    .AddGraphTypes()
+                    .AddDataLoader()
+                    .AddUserContextBuilder(httpContext => new Dictionary<string, object>
+                    {
+                        {"user", httpContext.User}
+                    })
+                );
+
+            AllowSynchronousIo(services);
+        }
+
+        //https://github.com/graphql-dotnet/graphql-dotnet/issues/1326
+        private static void AllowSynchronousIo(IServiceCollection services)
+        {
+            // kestrel
+            services.Configure<KestrelServerOptions>(options => { options.AllowSynchronousIO = true; });
+
+            // IIS
+            services.Configure<IISServerOptions>(options => { options.AllowSynchronousIO = true; });
+        }
+    }
+}
---
+++ aspnet-core/src/MyCompanyName.AbpZeroTemplate.GraphQL/Schemas/MainSchema.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Dependency;
+using GraphQL.Types;
+using MyCompanyName.AbpZeroTemplate.Queries.Container;
+using System;
+using GraphQL.Conversion;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyCompanyName.AbpZeroTemplate.Schemas
+{
+    public class MainSchema : Schema, ITransientDependency
+    {
+        public MainSchema(IServiceProvider provider) :
+            base(provider)
+        {
+            Query = provider.GetRequiredService<QueryContainer>();
+            NameConverter = new CamelCaseNameConverter();
+        }
+    }
+}
